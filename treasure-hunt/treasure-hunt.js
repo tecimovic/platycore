879abd1abb88500044a21235ea27a81014ec007f
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2021-07-03 12:22:14 -0400

    Rename treasure-hunt.

--- treasure-hunt/treasure-hunt.js
+++ treasure-hunt/treasure-hunt.js
@@ -2,14 +2,6 @@
  * A script that takes a JSON file of clues and produces a treasure hunt out of it.
  * See one of the JSON files for example.
  *
- * You should also print ascii table for the hunter, as this is essentially ascii encoding of
- * the text with a simple arithmetic modifier.
- *
- * JSON file contains an array of objects that should contain:
- *    "spot" - instruction for the parent where to put the next clue.
- *    "clue" - clue for the treasure hunter
- *    "op" - operation. Currently supported: "none", -, +, /, *
- *    "key" - number that is used by the operation.
  */
 const path = require('path')
 const fs = require('fs')
@@ -40,6 +32,32 @@ function encode(text, op, modifier) {
 }
 
 let input = process.argv[2]
+
+if (!input) {
+  console.log(`Usage: node treasure-hunt.js <JSON FILE>
+
+JSON file should contain an array of "step" objects, each step object
+needs to contain following data:
+  "spot" - instruction for the parent where to put the next clue.
+  "clue" - clue for the treasure hunter
+  "op" - operation. Currently supported: "none", -, +, /, *
+  "key" - number that is used by the operation.
+
+This program will print out the instructions for the parent where
+to put the clues and the clues themselves. You should probably
+copy these into google doc or something and decorate them a bit...
+
+You should also print ascii table for the hunter, as this is essentially
+an ASCII encoding of the text with a simple arithmetic modifier.
+
+The goal is:
+  - hunter learns what ASCII codes are
+  - hunter does decent amount of math to get to the goal
+  - hunter is hopefully not frustrated to the point of crying.... :)
+`)
+  process.exit(0)
+}
+
 let hunt = JSON.parse(fs.readFileSync(input))
 
 console.log('Instructions for parent:')
